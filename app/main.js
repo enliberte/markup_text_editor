1822f32176a03286c39e4bf1a36ee5600cbbf886
Alexey Sumatokhin <Alexey.Sumatokhin@mpicloud.com>
2023-04-19 18:35:32 +0300

    first

---
+++ app/main.js
@@ -0,0 +1,23 @@
+const { app, BrowserWindow } = require("electron");
+
+let mainWindow = null;
+
+app.on("ready", () => {
+  mainWindow = new BrowserWindow({
+    show: false,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  mainWindow.loadFile("app/index.html");
+
+  mainWindow.on("ready-to-show", () => {
+    mainWindow.show();
+  });
+
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+});
